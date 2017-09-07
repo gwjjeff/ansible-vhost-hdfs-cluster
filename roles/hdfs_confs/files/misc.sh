5f7f6c287c1e67d5fd5bf2115b67706e2a8332cf
gwjjeff <gwjjeff@gmail.com>
2017-09-07 23:40:55 +0800

    1st commit

---
+++ roles/hdfs_confs/files/misc.sh
@@ -0,0 +1,19 @@
+export JAVA_HOME=/opt/jdk1.8.0_144
+export PATH=$JAVA_HOME/bin:$PATH
+
+export HADOOP_PREFIX=/opt/hadoop-2.7.4
+export HADOOP_HOME=$HADOOP_PREFIX
+
+export HADOOP_CONF_DIR=$HOME/conf
+export HADOOP_LOG_DIR=$HOME/log
+export HADOOP_PID_DIR=$HOME/pid
+
+export HADOOP_MAPRED_LOG_DIR=$HOME/log
+export HADOOP_MAPRED_PID_DIR=$HOME/pid
+
+export YARN_CONF_DIR=$HOME/conf
+export YARN_LOG_DIR=$HOME/log
+export YARN_PID_DIR=$HOME/pid
+
+export HADOOP_HEAPSIZE=400
+export YARN_HEAPSIZE=400
